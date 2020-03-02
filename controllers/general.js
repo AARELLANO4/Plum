dac197cc3239126f8ea653d3ba5e27b295336c23
Alexis Arellano <aarellano596@gmail.com>
2020-03-02 13:04:05 -0500

    assign 2 updates

--- controllers/general.js
+++ controllers/general.js
@@ -51,45 +51,67 @@ router.get("/registration",(req,res)=>{
 // process registration form when user submits
 router.post("/registration", (req,res)=> {
 
-   // const {firstName,lastName,email,password,rePassword} = req.body;
+   const {firstName,lastName,email,password,rePassword} = req.body;
 
-    const errorMessages = [];
+    const passwordErr = [];
+    let fnErr = [];
+    let lnErr = [];
+    let emailErr = [];
+    let count = 0;
 
-    if (req.body.firstName == "") {
-        errorMessages.push("You must enter your first name."); 
+    if (firstName == "") {
+        fnErr += "You must enter your first name."; 
+        count += 1;
     }
     
-    if (req.body.lastName == "") {
-        errorMessages.push("You must enter your last name.");
+    if (lastName == "") {
+        lnErr += "You must enter your last name.";
+        count += 1;
     }
 
-    if (req.body.email == "") {
-        errorMessages.push("You must enter your email."); 
+    if (email == "") {
+        emailErr += "You must enter your email."; 
+        count += 1;
     }
 
-    if (req.body.password == "") {
-        errorMessages.push("You must enter your password.");
+    if (password == "") {
+        passwordErr.push("You must enter your password.");
+        count += 1;
     }
     
-    if (req.body.rePassword == "") {
-        errorMessages.push("You must re-enter your password."); 
+    if (rePassword == "") {
+        passwordErr.push("You must re-enter your password."); 
+        count += 1;
     }
 
     
-    if (req.body.rePassword != req.body.password) {
-        errorMessages.push("Your passwords must match.");
+    if (rePassword != password) {
+        passwordErr.push("Your passwords must match.");
+        count += 1;
     }
 
-    if (req.body.password.length < 8) {
-        errorMessages.push("Your password must be at least 8 characters");
+    if (password.length < 6) {
+        passwordErr.push("Your password must be at least 6-12 characters long.");
+        count += 1;
     }
 
-    if (errorMessages.length > 0) {
+  /*  if (password.match(/^[A-Za-z0-9]+$/) == null && rePassword.match(/^[A-Za-z0-9]+$/)) {
+        passwordErr.push("Password should only contain numbers and letters.");
+    }*/
+
+    if (password.match(/^[A-Za-z]+$/) == null || rePassword.match(/^[0-9]+$/ == null)) {
+        passwordErr.push("Password should only contain numbers and letters.");
+    }
+
+    if (count > 0) {
         res.render("registration", {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             email: req.body.email,
-            errors: errorMessages
+            fnErr: fnErr,
+            lnErr: lnErr,
+            emailErr: emailErr,
+            passwordErr: passwordErr
         });
     }
     else {
@@ -102,33 +124,35 @@ router.post("/registration", (req,res)=> {
             email: email,
             success: `Thank you ${firstName} ${lastName}, for joining Plum! We will send you an email shortly to validate your email address.`
         });
-    }
-    /*
-    // using Twilio SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey("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");
-    
-    const msg = {
-    to: `aarellano596@gmail.com`,
-    from: `${email}`,
-    subject: `Registration Confirmation`,
-    html: 
-    `
-        Hi ${firstName} ${lastName}!
-        Thank you for choosing <b>Plum!</b>
-    `,
-    };
 
-    // asynchronous operation 
-    sgMail.send(msg)
-    .then(()=> {
-        res.redirect("/");
-    })
-    .catch(err=>{
-        console.log(`Error ${err}`);
-    })
-    */
+            // using Twilio SendGrid's v3 Node.js Library
+            // https://github.com/sendgrid/sendgrid-nodejs
+            const sgMail = require('@sendgrid/mail');
+            sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+            
+            const msg = {
+            to: `${email}`,
+            from: `aarellano4@myseneca.ca`,
+            subject: `Registration Confirmation`,
+            html: 
+            `
+                Hi ${firstName} ${lastName}!
+                Thank you for choosing <b>Plum!</b>
+            `,
+            };
+
+            // asynchronous operation 
+            sgMail.send(msg)
+            .then(()=> {
+                res.redirect("/dashboard");
+            })
+            .catch(err=>{
+                console.log(`Error ${err}`);
+            })
+    }
+    
+
+    
 });
 
 // login route
@@ -148,21 +172,27 @@ router.post("/login",(req,res)=> {
 
     const {email,password} = req.body;
  
-    const errorMessages = [];
+    let emailErr = [];
+    let count = 0;
  
     if (email == "") {
-        errorMessages.push("Email required to log in.")
+        emailErr += "Email required to log in.";
+        count++;
     }
 
+    let passwordErr = [];
+
     if (password == "") {
-        errorMessages.push("Password required to log in.")
+        passwordErr += "Password required to log in.";
+        count++;
     }
 
  
-     if (errorMessages.length > 0) {
+     if (count > 0) {
          res.render("login", {
              email: email,
-             errors: errorMessages
+             emailErr: emailErr,
+             passwordErr: passwordErr
          });
      }
      else {
